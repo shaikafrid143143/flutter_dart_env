5bf6d4a92a81467626ef10c8ba2dba4b28e34c57
AFRID SHAIK <afridayan01@gmail.com>
2023-09-12 14:25:07 +0000

    1.1.4

--- lib/email_sender.dart
+++ lib/email_sender.dart
@@ -31,25 +31,25 @@ class Email{
 
 -> USAGE EMAIL SENDER
 
-  1. Initalize emailSender
-  2. Initalize a method from emailSender
+  1. Initalize EmailSender
+  2. Initalize a method from EmailSender
   3. Get output
 
 -> Rules for usage
 
   4. toEmail must be initalize for sending email
-  5. You cannot use password to send custom message you need to provide passkey , for more. visit : https://support.google.com/accounts/answer/13548313?hl=en
+  5. You cannot use password to send custom message you need to provide app password , for more. visit : https://support.google.com/accounts/answer/13548313?hl=en
   6. If you dont fill other fileds api will automatically fill default data except toEmail
 
 -> Examples usage
 
-  > emailSender emailsender = emailSender();
+  > EmailSender emailsender = EmailSender();
   > var responseFromEmailSender = await emailsender.details();
   > print(responseFromEmailSender);
 
 -> Methods
 
-  > details()
+
   > send()
   > sendOtp()
   > sendMessage()
