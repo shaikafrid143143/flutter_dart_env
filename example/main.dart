21bec99077acb8e68281d9d5513e2856c7c97bf9
AFRID SHAIK <afridayan01@gmail.com>
2023-09-12 12:19:42 +0000

    Create main.dart

---
+++ example/main.dart
@@ -0,0 +1,15 @@
+import 'package:email_sender/email_sender.dart';
+
+void main() async {
+  //initalize EmailSender class
+  EmailSender emailSender = EmailSender();
+  //initalize send method to response variable
+  var response = await emailSender.send("@gmail.com");
+  if (response["message"] == "emailSendSuccess") {
+    print(response);
+  } else {
+    print("something Failed");
+    //for understanding the error 
+    print(response);
+  }
+}
